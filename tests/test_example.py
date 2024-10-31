f4d2bf087f4be81842df2db1a57cb1495bab7964
Alexandr Savelyev <alexandr.savelyev@saritasa.com>
2024-10-31 17:35:22 +0700

    Init project

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,25 +1,5 @@
-import pytest
-from playwright.sync_api import Playwright, sync_playwright
 from pages.example_page import ExamplePage
 
-@pytest.fixture(scope="session")
-def browser():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        yield browser
-        browser.close()
-
-@pytest.fixture(scope="session")
-def context(browser):
-    context = browser.new_context()
-    yield context
-    context.close()
-
-@pytest.fixture(scope="function")
-def page(context):
-    page = context.new_page()
-    yield page
-    page.close()
 
 def test_example(page):
     page_obj = ExamplePage(page)
