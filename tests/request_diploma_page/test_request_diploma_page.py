a40d163f48260967e7b9ef165e14f6dfdaa07847
Alexandr Savelyev <alexandr.savelyev@saritasa.com>
2024-12-12 21:41:00 +0700

    Add empty form test

--- tests/request_diploma_page/test_request_diploma_page.py
+++ tests/request_diploma_page/test_request_diploma_page.py
@@ -12,7 +12,7 @@ def test_form_visible(request_diploma_page: RequestDiplomaPage):
 
 def test_sending_form_request(request_diploma_page: RequestDiplomaPage):
     """Check that user can send diploma request."""
-    time.sleep(3)
+    time.sleep(2)
     request_diploma_page.last_name.fill("Фамилия")
     request_diploma_page.first_name.fill("Имя")
     request_diploma_page.surname.fill("Отчество")
@@ -38,10 +38,19 @@ def test_sending_form_request(request_diploma_page: RequestDiplomaPage):
     request_diploma_page.diploma_month.select_option("3")
     request_diploma_page.diploma_year.select_option("2004")
     request_diploma_page.take_by_myself.click()
-    time.sleep(3)
+    request_diploma_page.personal_data_checkbox.click()
+    time.sleep(2)
 
     request_diploma_page.confirm_request()
 
     expect(request_diploma_page.validation_message).to_contain_text(
         "Поле «Скан/фото диплома» обязательно для заполнения."
     )
+
+
+def test_empty_form_validation(request_diploma_page: RequestDiplomaPage):
+    """Check validations in request diploma form."""
+    request_diploma_page.confirm_request()
+    expect(request_diploma_page.validation_message).to_contain_text(
+        "Сообщение об ошибке Поле «Фамилия» обязательно для заполнения. Поле «Имя» обязательно для заполнения. Поле «Отчество» обязательно для заполнения. Поле «Дата рождения» обязательно для заполнения. Поле «E-mail» обязательно для заполнения. Поле «Телефон» обязательно для заполнения. Поле «Институт/филиал СФУ» обязательно для заполнения. Поле «Направление подготовки / специальность» обязательно для заполнения. Поле «Форма обучения» обязательно для заполнения. Поле «Год поступления» обязательно для заполнения. Поле «Год окончания обучения» обязательно для заполнения. Поле «Вид документа» обязательно для заполнения. Поле «Регистрационный номер диплома» обязательно для заполнения. Поле «Дата выдачи» обязательно для заполнения. Поле «Серия, номер диплома» обязательно для заполнения. Поле «Скан/фото диплома» обязательно для заполнения. Поле «По готовности документ будет получен» обязательно для заполнения. Поле «Персональные данные» обязательно для заполнения."
+        )
